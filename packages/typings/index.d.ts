62038284b70afd88d0a01bdda83693ae7825c81a
yiwwhl <yiwwhl@qq.com>
2023-04-14 18:23:29 +0800

    chore(typings): some new utility types have been added

--- packages/typings/index.d.ts
+++ packages/typings/index.d.ts
@@ -1,3 +1,23 @@
+// match any function
+declare type AnyFunction = (...args: any[]) => any;
+
+// match any object
+declare type AnyObject = Record<PropertyKey, any>;
+
+// macth object by T
+declare type TypedObject<T = any> = Record<PropertyKey, T>;
+
+// match: Xxx
+declare type AnyUpperCase = `${Uppercase<string>}${string}`;
+
+// match: xxx-xxx-xxx
+declare type AnyDateType = `${string}-${string}-${string}`;
+
+// match: xxx:xxx:xxx
+declare type AnyTimeType = `${string}:${string}:${string}`;
+
+// match any class instance
+declare type AnyClass<T> = new () => T;
 declare interface ImportMetaEnv {
   VITE_PROJECT_START_TIME: string;
 }
@@ -20,3 +40,9 @@ declare interface CustomContext {
 };
 
 declare type ResponseCreator = (data: any, message: any, code: ResponseCode) => ResponseBody;
+/**
+ * description:
+ * const arr = ['1', 2, '3']
+ * type ArrType = UnwrapArray<typeof arr> => string | number
+ */
+declare type UnwrapArray<T> = T extends Array<infer P> ? P : never;
