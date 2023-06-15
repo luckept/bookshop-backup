42cf251b0a38aeaf5bb2cbd93fdcf1046522335a
yiwwhl <yiwwhl@qq.com>
2023-06-15 21:53:29 +0800

    feat(typings): add common enum LayoutDirection

--- packages/typings/enum.ts
+++ packages/typings/enum.ts
@@ -2,6 +2,11 @@ export enum ProjectEnv {
   DEV = "development",
   PROD = "production",
 }
+
+export enum LayoutDirection {
+  VERTICAL = "vertical",
+  HORIZONTAL = "horizontal",
+}
 export enum ResponseCode {
   SUCCESS = 200,
   SERVERERROR = 500,
--- packages/typings/modules/common/enum.ts
+++ packages/typings/modules/common/enum.ts
@@ -2,3 +2,8 @@ export enum ProjectEnv {
   DEV = "development",
   PROD = "production",
 }
+
+export enum LayoutDirection {
+  VERTICAL = "vertical",
+  HORIZONTAL = "horizontal",
+}
